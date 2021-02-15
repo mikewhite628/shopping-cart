c7580d574f15d762d353d881031938cd29ec1af8
mikewhite628 <mikewhite628@gmail.com>
2021-02-15 16:34:52 -0600

    finished styling for now

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import Routes from './Routes';
 import {HashRouter as Router} from 'react-router-dom'
+import Home from './Components/Home/Home';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-    <Routes />
+    <Home />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
